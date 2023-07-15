357560ce530a0cfa64f93749e61ae97656d9cfa1
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-15 15:41:18 +0300

    ADD: google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
+import GoogleAnalytics from '@bradgarropy/next-google-analytics/';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+
+      <GoogleAnalytics measurementId="G-QK5JQ327KP" />
     </html>
   );
 }
