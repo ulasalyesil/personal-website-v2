71d2700bf4e0d6110d23960db993d272b76192a3
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-16 02:14:19 +0300

    FIX: animated type cursor color

--- components/TypingAnimatedText.jsx
+++ components/TypingAnimatedText.jsx
@@ -35,7 +35,7 @@ export default function TypingAnimatedText({ text }) {
         {displayedText ? displayedText : text}
       </motion.span>
       <motion.span
-        className="ml-1 inline-flex h-[22px] w-[2px] animate-blink rounded-full bg-neutral-200 align-sub opacity-75 animate-pulse"
+        className="ml-1 inline-flex h-[22px] w-[2px] animate-blink rounded-full bg-neutral-200 dark:bg-neutral-800 align-sub opacity-75 animate-pulse"
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true}}
