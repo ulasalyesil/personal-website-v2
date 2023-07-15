261e2375a306b76a09bfb617f6158bc7e3c7409d
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-15 17:43:50 +0300

    FIX: project card icon

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -8,14 +8,14 @@ function ProductCard({ target, icon, title, img }) {
   return (
     <Link
       href={target}
-      className="group relative overflow-hidden w-full aspect-square bg-neutral-900 p-4 rounded-3xl text-left cursor-pointer drop-shadow-md transition ease-out delay-50 md:hover:drop-shadow-xl md:hover:scale-105"
+      className="group relative overflow-hidden w-full aspect-square bg-neutral-900 p-4 rounded-3xl text-left cursor-pointer drop-shadow-md transition ease-in-out delay-50 md:hover:drop-shadow-xl md:hover:scale-105"
     >
       <Image
         src={img}
         alt={title}
         className="absolute -z-20 bg-blend-multiply w-full object-contain inset-0"
       />
-      <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-neutral-900 to-neutral-900/5 -z-10"></div>
+      <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-neutral-900 to-neutral-900/5 -z-10 overflow-hidden"></div>
       <div className="flex justify-between items-end h-full relative overflow-hidden">
         <h3 className="font-medium text-neutral-100 ">{title}</h3>
         <Image
@@ -23,7 +23,7 @@ function ProductCard({ target, icon, title, img }) {
         alt="icon"
         priority={true}
         aria-label="hidden"
-        className="hidden transition group-hover:block"
+        className="hidden transition group-hover:block absolute top-1 right-1 -rotate-45 rounded-full" 
         width={'20'}
         height={'20'}
       />
