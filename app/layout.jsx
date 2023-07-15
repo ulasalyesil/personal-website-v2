0db443a92041340d050ead28bcedf72384a50945
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-16 00:05:42 +0300

    ADD: bottom & top gradient

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div>
+        <div className='bg-gradient-to-b from-neutral-100 dark:from-neutral-950 to-neutral-100/0 dark:neutral-950/0 w-screen h-40 fixed top-0'/>
           {children}
+          <div className='bg-gradient-to-t from-neutral-100 dark:from-neutral-950 to-neutral-100/0 dark:neutral-950/0 w-screen h-40 fixed bottom-0'/>
         </div>
         </body>
 
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,17 +5,16 @@ import { TwitterLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col gap-2 items-center justify-end w-full h-[192px] py-4 pb-8 bottom-0 selection:text-neutral-100 selection:bg-[#FF6100]">
+    <footer className="flex flex-col gap-2 items-center pt-32 pb-40 justify-center w-full h-40 bottom-0 selection:text-neutral-100 selection:bg-[#FF6100]">
       <div className="text-neutral-500 flex  gap-2">
         <p>Reach me at</p>
         <a href="https://twitter.com/ulasalyesil" target="_blank">
-            {/* change to next <Image/> */}
-          <TwitterLogoIcon width={'24'} height={'24'}/>
+          {/* change to next <Image/> */}
+          <TwitterLogoIcon width={"24"} height={"24"} />
         </a>
         <p>or</p>
         <a href="https://linkedin.com/in/ulasalyesil" target="_blank">
-            
-          <LinkedInLogoIcon width={'24'} height={'24'}/>
+          <LinkedInLogoIcon width={"24"} height={"24"} />
         </a>
       </div>
       <p className="text-neutral-500">
