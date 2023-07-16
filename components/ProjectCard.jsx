2c290d57421c597be6fe229e67f486ac0455ead1
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-16 17:33:38 +0300

    FIX: projectCard icon update

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -15,12 +15,12 @@ function ProductCard({ target, icon, title, img }) {
         alt={title}
         className="absolute -z-20 bg-blend-multiply w-full object-contain inset-0"
       />
-      <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-neutral-100 dark:from-neutral-900 to-neutral-900/5 -z-10 overflow-hidden md:hover:scale-105 transition ease-in-out delay-50"></div>
+      <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t backdrop-blur-sm group-hover:backdrop-blur-none from-neutral-100 dark:from-neutral-900 to-neutral-900/5 -z-10 overflow-hidden md:hover:scale-105 transition ease-in-out delay-50"></div>
       <div className="flex justify-between items-end h-full relative overflow-hidden">
         <h3 className="font-semibold text-neutral-500 dark:text-neutral-100">
           {title}
         </h3>
-        <div className="p-2 bg-neutral-100/50 dark:bg-neutral-900/50 backdrop-blur-sm hidden transition ease-in-out delay-50 group-hover:block absolute top-[2px] right-[2px] rounded-full">
+        <div className="p-2 bg-neutral-100/50 dark:bg-neutral-900/50 backdrop-blur-sm hidden transition ease-in-out delay-50 group-hover:block hover:bg-neutral-100 absolute top-[2px] right-[2px] rounded-full">
           <ArrowTopRightIcon
             width={"20"}
             height={"20"}
