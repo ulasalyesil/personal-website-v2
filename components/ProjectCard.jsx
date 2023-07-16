d0c87e8f00487388ce50b74ab984cd4dd7a38804
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-16 19:19:38 +0300

    FIX: safari hover bugs

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -17,7 +17,7 @@ function ProductCard({ target, icon, title, img }) {
   return (
     <Link
       href={target}
-      className={`group relative overflow-hidden w-full aspect-square bg-neutral-100 dark:bg-neutral-900 p-4 rounded-3xl text-left cursor-pointer ${
+      className={`group relative overflow-hidden w-full aspect-square p-4 rounded-3xl text-left cursor-pointer bg-neutral-100 dark:bg-neutral-900  border border-neutral-200 dark:border-neutral-800 ${
         isSafari ? "" : "drop-shadow-md"
       } transition ease-in-out delay-50 ${
         isSafari ? "" : "md:hover:drop-shadow-xl"
@@ -26,16 +26,14 @@ function ProductCard({ target, icon, title, img }) {
       <Image
         src={img}
         alt={title}
-        className={`absolute ${
-          isSafari ? "" : "-z-20"
-        } bg-blend-multiply w-full object-contain inset-0`}
+        className="absolute object-contain inset-0"
       />
-      <div className={`absolute bottom-0 left-0 w-full h-full bg-gradient-to-t ${isSafari ? "" : "md:backdrop-blur-sm"} group-hover:backdrop-blur-none from-neutral-100 dark:from-neutral-900 to-neutral-900/5 -z-10 overflow-hidden md:hover:scale-105 transition ease-in-out delay-50`}></div>
+      <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-neutral-100 dark:from-neutral-900 md:backdrop-blur-sm group-hover:backdrop-blur-none transition ease-in-out md:group-hover:scale-105 overflow-hidden inset-0" />
       <div className="flex justify-between items-end h-full relative overflow-hidden">
         <h3 className="font-semibold text-neutral-500 dark:text-neutral-100">
           {title}
         </h3>
-        <div className="p-2 bg-neutral-100/50 dark:bg-neutral-900/50 backdrop-blur-sm hidden transition ease-in-out delay-50 group-hover:block hover:bg-neutral-100 absolute top-[2px] right-[2px] rounded-full">
+        <div className="p-2 bg-neutral-100/50 dark:bg-neutral-900/50 backdrop-blur-sm hidden transition ease-in-out delay-50 group-hover:block hover:bg-neutral-100 dark:hover:bg-neutral-800 absolute top-[2px] right-[2px] rounded-full">
           <ArrowTopRightIcon
             width={"20"}
             height={"20"}
