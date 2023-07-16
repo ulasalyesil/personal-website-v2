b49dd7a6ec4e1d1c64cfb343f705e95b779c9e2b
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-16 18:44:09 +0300

    FIX: safari filters bug on projectCard

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -2,20 +2,35 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 
 function ProductCard({ target, icon, title, img }) {
+
+  const [isSafari, setIsSafari] = useState(false);
+
+  useEffect(() => {
+    const isSafariBrowser = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    setIsSafari(isSafariBrowser);
+  }, []);
+
   return (
     <Link
       href={target}
-      className="group relative overflow-hidden w-full aspect-square bg-neutral-100 dark:bg-neutral-900  p-4 rounded-3xl text-left cursor-pointer drop-shadow-md transition ease-in-out delay-50  md:hover:drop-shadow-xl md:hover:scale-105"
+      className={`group relative overflow-hidden w-full aspect-square bg-neutral-100 dark:bg-neutral-900 p-4 rounded-3xl text-left cursor-pointer ${
+        isSafari ? "" : "drop-shadow-md"
+      } transition ease-in-out delay-50 ${
+        isSafari ? "" : "md:hover:drop-shadow-xl"
+      } md:hover:scale-105`}
     >
       <Image
         src={img}
         alt={title}
-        className="absolute -z-20 bg-blend-multiply w-full object-contain inset-0"
+        className={`absolute ${
+          isSafari ? "" : "-z-20"
+        } bg-blend-multiply w-full object-contain inset-0`}
       />
-      <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t backdrop-blur-sm group-hover:backdrop-blur-none from-neutral-100 dark:from-neutral-900 to-neutral-900/5 -z-10 overflow-hidden md:hover:scale-105 transition ease-in-out delay-50"></div>
+      <div className={`absolute bottom-0 left-0 w-full h-full bg-gradient-to-t ${isSafari ? "" : "md:backdrop-blur-sm"} group-hover:backdrop-blur-none from-neutral-100 dark:from-neutral-900 to-neutral-900/5 -z-10 overflow-hidden md:hover:scale-105 transition ease-in-out delay-50`}></div>
       <div className="flex justify-between items-end h-full relative overflow-hidden">
         <h3 className="font-semibold text-neutral-500 dark:text-neutral-100">
           {title}
