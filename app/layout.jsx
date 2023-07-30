bf59e22691e8086cc3e47f4d26bec61239f67da4
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-30 12:56:52 +0300

    ADD: preview meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,31 @@
-import '../styles/globals.css'
-import { Inter } from 'next/font/google'
-import GoogleAnalytics from '@bradgarropy/next-google-analytics/';
-import { Analytics } from '@vercel/analytics/react';
-import Header from '@/components/Header';
+// next
+import { Inter } from "next/font/google";
 
+// anayltics
+import GoogleAnalytics from "@bradgarropy/next-google-analytics/";
+import { Analytics } from "@vercel/analytics/react";
 
+// styles
+import "../styles/globals.css";
+// component
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Ulaş Alyeşil | Product Designer',
-  description: 'Designing for memorable feelings.',
-}
+  title: "Ulaş Alyeşil | Product Designer",
+  description: "Designing for memorable feelings.",
+  ogImage: "@/public/images/preview.png",
+  twitterImage: "@/public/images/preview.png",
+  ogImageAlt: "About Ulaş Alyeşil",
+  twitterImageAlt: "About Ulaş Alyeşil",
+};
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='flex flex-col m-auto items-center pt-12 sm:pt-32'>
+        <div className="flex flex-col m-auto items-center pt-12 sm:pt-32">
           <Header />
           <div className="bg-gradient-to-b from-neutral-100 dark:from-neutral-950 to-neutral-100/0 dark:neutral-950/0 w-screen h-24 sm:h-40 fixed top-0" />
           {children}
