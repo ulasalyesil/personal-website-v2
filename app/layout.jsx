49c006873bb2125791a490d85575852b05d5d02b
Ulaş Alyeşil <umutulas08@gmail.com>
2023-07-30 13:03:19 +0300

    TRY: meta imgae

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,44 +15,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ulaş Alyeşil | Product Designer",
-  description: "Designing for memorable feelings.",
-  ogImage: "@/public/images/preview.png",
-  twitterImage: "@/public/images/preview.png",
-  ogImageAlt: "About Ulaş Alyeşil",
-  twitterImageAlt: "About Ulaş Alyeşil",
+  description: "Designing for memorable feelings.", 
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Open Graph / Facebook */}
-        {metadata.ogImage && (
-          <meta property="og:image" content={metadata.ogImage} />
-        )}
-        {metadata.ogImage && (
-          <meta property="og:image:type" content="image/jpeg" />
-        )}
-        {metadata.ogImage && (
-          <meta property="og:image:width" content="<your_image_width>" />
-        )}{" "}
-        {/* Replace with the actual width */}
-        {metadata.ogImage && (
-          <meta property="og:image:height" content="<your_image_height>" />
-        )}{" "}
-        {/* Replace with the actual height */}
-        {metadata.ogImageAlt && (
-          <meta property="og:image:alt" content={metadata.ogImageAlt} />
-        )}
-        {/* Twitter */}
-        {metadata.twitterImage && (
-          <meta name="twitter:image" content={metadata.twitterImage} />
-        )}
-        {metadata.twitterImageAlt && (
-          <meta name="twitter:image:alt" content={metadata.twitterImageAlt} />
-        )}
+        <meta property="og:image" content="@public/images/preview.png"/>
+        <meta property="twitter:image" content="@public/images/preview.png"/>
       </Head>
       <body className={inter.className}>
         <div className="flex flex-col m-auto items-center pt-12 sm:pt-32">
