09ee81c90913870cde1a0e20a9d124a65b4a2156
ulasalyesil <ulasalyesil@gmail.com>
2023-09-10 00:04:19 +0300

    FIX: 404 page typo

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -5,7 +5,7 @@ import ContactCard from '@/components/ContactCard';
 export default function NotFound() {
   return (
     <div className="text-neutral-900 dark:text-neutral-100 flex flex-col m-auto p-auto justify-center items-center h-screen gap-4">
-      <h1 className="text-2xl font-bold max-w-[556px] text-center">We regret to inform that we decided to move on with other pages. We'll keep your candidate info and reach out if we see any fit.</h1>
+      <h1 className="text-2xl font-bold max-w-[556px] text-center">We regret to inform that we decided to move on with other pages. We&apos;ll keep your candidate info and reach out if we see any fit.</h1>
       <div className='flex gap-4 items-center'>
         <p>Go</p>
           <Button href={'/'} label={"Home"} type={"primary"} />
