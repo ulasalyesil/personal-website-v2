959473ed214a04a327edf3ed1238dd4ed4481257
ulasalyesil <ulasalyesil@gmail.com>
2023-10-11 23:44:24 +0300

    FIX: secondary button dark style

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -9,7 +9,7 @@ function Button({ type, label, href, target }) {
       primaryLong:
       "h-8 flex items-center text-neutral-100  md:text-base bg-neutral-700 hover:bg-neutral-900 w-full font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-neutral-700 dark:hover:bg-neutral-500 dark:focus:ring-neutral-800 transition ease-in-out",
     secondary:
-      "h-8 flex items-center text-neutral-500 bg-neutral-200 dark:bg-neutral-800 text-xs md:text-base rounded-lg text-sm font-medium px-4 py-2 hover:text-neutral-700 hover:bg-neutral-100 focus:z-10 dark:text-neutral-300 dark:hover:text-white dark:hover:bg-neutral-800/50 transition ease-in-out",
+      "h-8 flex items-center text-neutral-500 bg-neutral-200 dark:bg-neutral-900 dark:border border-neutral-700 text-xs md:text-base rounded-lg text-sm font-medium px-4 py-2 hover:text-neutral-700 hover:bg-neutral-100 focus:z-10 dark:text-neutral-300 dark:hover:text-white dark:hover:bg-neutral-800/50 transition ease-in-out",
   };
 
   return (
