576e2d80e4c287a4a82766735171a16db25f05b6
Ulaş Alyeşil <umutulas08@gmail.com>
2025-07-16 16:36:18 +0200

    footer icon fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,18 @@
 'use client'
 
 import React from "react";
-import { TwitterLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { LinkedInLogoIcon } from "@radix-ui/react-icons";
+import XIcon from "../public/icons/contactIcons/x";
 
 export default function Footer() {
   return (
     <footer className="flex flex-col gap-2 items-center pt-32 pb-80 justify-center w-full h-40 bottom-0 selection:text-neutral-100 selection:bg-[#FF6100]">
       <div className="text-neutral-500 flex  gap-2">
-        <p>Reach me at</p>
+        <p>reach me at</p>
         <a href="https://twitter.com/ulasalyesil" target="_blank">
           {/* change to next <Image/> */}
-          <TwitterLogoIcon width={"24"} height={"24"} />
+          {/* <TwitterLogoIcon width={"24"} height={"24"} /> */}
+          <XIcon  fill={'#000'}/>
         </a>
         <p>or</p>
         <a href="https://linkedin.com/in/ulasalyesil" target="_blank">
@@ -18,13 +20,13 @@ export default function Footer() {
         </a>
       </div>
       <p className="text-neutral-500">
-        Also, see my{" "}
+        also, see my{" "}
         <a
           className="font-semibold hover:text-[#017bfc]"
           href="https://bento.me/ulas"
           target="_blank"
         >
-          Bento
+          bento
         </a>
       </p>
       <a href="mailto:hello@ulasalyesil.com" className="text-neutral-500 font-medium hover:text-neutral-900 hover:decoration-slice">hello@ulasalyesil.com</a>
