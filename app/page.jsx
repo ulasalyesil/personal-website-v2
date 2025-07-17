a923a3b968f1ba9654b58f47b747993a2797079e
Ulaş Alyeşil <umutulas08@gmail.com>
2025-07-17 17:03:13 +0200

    dark bg update

--- app/page.jsx
+++ app/page.jsx
@@ -22,7 +22,7 @@ const motionProps = {
 function Page() {
   return (
     <div className="text-sm md:text-base">
-      <motion.div className="flex flex-col sm:p-4 sm:m-auto rounded-xl sm:w-[1200px] gap-16 mb-auto bg-neutral-100/20 dark:bg-neutral-950">
+      <motion.div className="flex flex-col sm:p-4 sm:m-auto rounded-xl sm:w-[1200px] gap-16 mb-auto">
         <Hero />
         <motion.ol
           className="flex flex-col gap-16 rounded-xl py-4 selection:bg-slate-100 selection:text-[#017BFC]"
